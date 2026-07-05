07a9e151a124e8c2910fbe91989c51a3815764a1
Sooraj Rajagopalan <sooraj@tuvl.io>
2026-07-05 13:50:44 +0100

    chore(release): v2026.2.6.1 version pill

--- src/data/version.js
+++ src/data/version.js
@@ -1,4 +1,4 @@
-export const VERSION_TAG = 'v2026.2.6';
+export const VERSION_TAG = 'v2026.2.6.1';
 
 const SEMVER = VERSION_TAG.replace(/^v/, '');
 const IS_PRERELEASE = /-(beta|alpha|rc)\./.test(SEMVER);
